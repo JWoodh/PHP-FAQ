690fb7e2adc27ca742196776d35d37a0e353ceac
JWoodh <jowoa002@osloskolen.no>
2023-06-05 11:01:23 +0200

    check hostname

--- conn.php
+++ conn.php
@@ -1,3 +1,3 @@
 <?php 
-$kobling = mysqli_connect('10.10.43.43', 'java', '010500', 'java')
+$kobling = mysqli_connect('jwoodh', 'java', '010500', 'java')
                 or die('Error connecting to MySQL server.');
